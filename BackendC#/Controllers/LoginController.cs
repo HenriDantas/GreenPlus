80e82259c64d3ac12b91644d55e22c57ac2f3046
Henri Dantas <santoshenri0@gmail.com>
2024-10-07 19:16:52 -0300

    mudanças teste backend

---
+++ BackendC#/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackGreenPlus.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            if (request.Username == "admin" && request.Password == "admin")
+            {
+                return Ok(new { token = "  " });
+            }
+            return Unauthorized();
+        }
+    }
+
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
